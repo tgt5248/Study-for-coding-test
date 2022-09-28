d64392c559fd9f8d7f44857eadebbf2b6a634719
tgt5248 <63574571+tgt5248@users.noreply.github.com>
2022-09-29 00:20:04 +0900

    Create ppap.py

---
+++ Stack,Que/ppap.py
@@ -0,0 +1,48 @@
+"""
+Case 1 
+시간초과
+"""
+# import sys
+# str= str(sys.stdin.readline().strip())
+# while('PPAP' in str):
+#     str = str.replace('PPAP','P')
+# if str=='P':
+#     print('PPAP')
+# else:
+#     print('NP') 
+
+"""
+Case2 
+시간초과 
+"""
+
+# import sys
+# str= str(sys.stdin.readline().strip())
+
+# new=''
+# for i in str:
+#     new+=i
+#     if len(new)>=4 and new[-4:]=='PPAP':
+#         new=new[:-3]
+# if new=='P' or new=='':
+#     print('PPAP')
+# else:
+#     print('NP') 
+
+#------------------------------
+import sys
+str= str(sys.stdin.readline().strip())
+
+new=[]
+for i in str:
+    new.append(i)
+    if len(new)>=4 and new[-4:]==['P','P','A','P']:
+        for j in range(3):
+            new.pop()
+        # new=new[:-3]
+if new==['P'] :
+    print('PPAP')
+else:
+    print('NP') 
+    
+#new=new[:-3] 보다 pop3번하는게 빠른가보네 배우고 갑니다 
